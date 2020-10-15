a2d43203e2157c758ec040fb59f1a1d88aecb9dd
TravisHeffelfinger <travisjohn1011@gmail.com>
2020-10-15 05:07:45 -0600

    update

--- messagingapp/src/services/firebase.js
+++ messagingapp/src/services/firebase.js
@@ -1,5 +1,16 @@
 import firebase from 'firebase'
-import FIREBASE from '../keys'
+//import FIREBASE from '../keys'
+
+const FIREBASE = {
+    apiKey: "AIzaSyAHzS-BpuOpzmNVs6fcyGwP5_9C-38tx-g",
+    authDomain: "classroomchatter-a7f90.firebaseapp.com",
+    databaseURL: "https://classroomchatter-a7f90.firebaseio.com",
+    projectId: "classroomchatter-a7f90",
+    storageBucket: "classroomchatter-a7f90.appspot.com",
+    messagingSenderId: "416988184247",
+    appId: "1:416988184247:web:e25ee91765ef93607ed732",
+    measurementId: "G-CR305YY5T9"
+}
 
 const firebaseConfig = FIREBASE
 
